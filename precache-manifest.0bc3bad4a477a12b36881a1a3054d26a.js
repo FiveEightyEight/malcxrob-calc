612bd7c19bed5f8d458cf4b5ec7e8ff9f7b36d38
Robert Abreu <robertabreu@pursuit.org>
2019-02-10 11:09:38 -0500

    Updates

---
+++ precache-manifest.0bc3bad4a477a12b36881a1a3054d26a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "51916e9f61a323b49c81",
+    "url": "/malcxrob-calcBuild/static/js/runtime~main.51916e9f.js"
+  },
+  {
+    "revision": "952c06d0f7a21b1bbe86",
+    "url": "/malcxrob-calcBuild/static/js/main.952c06d0.chunk.js"
+  },
+  {
+    "revision": "6d8e1c317b730a1237c1",
+    "url": "/malcxrob-calcBuild/static/js/1.6d8e1c31.chunk.js"
+  },
+  {
+    "revision": "6d8e1c317b730a1237c1",
+    "url": "/malcxrob-calcBuild/static/css/1.9ec8e4e8.chunk.css"
+  },
+  {
+    "revision": "035e7608fd783c8cf292057a58ac4461",
+    "url": "/malcxrob-calcBuild/index.html"
+  }
+];
